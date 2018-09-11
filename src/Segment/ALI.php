44ca3f96279b8c679b23b1cee54e9e3f20b31218
Robert <robert@zagis.nl>
2018-09-11 14:29:41 +0200

    Package separately
    Signed-off-by: Robert <robert@zagis.nl>

--- src/Segment/ALI.php
+++ src/Segment/ALI.php
@@ -3,10 +3,12 @@ namespace PharmaIntelligence\GS1EDI\Segment;
 
 class ALI extends Segment
 {
+    
     const PARTIAL_DELIVERY_PROHIBITED   = 'X1';
     const PARTIAL_DELIVERY_ALLOWED      = 'X2';
+    const PACKAGE_SEPERATELY            = 'SEP';
     
-    public $partialDeliveryAllowed = null;
+    
     
     protected function map() {
         $this->partialDeliveryAllowed = ($this->components['code_scenario'] == self::PARTIAL_DELIVERY_ALLOWED);
